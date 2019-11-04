0cb8b4d51a88d77bfef028c109f1f801ca193ad5
SirawichDev <sirawit0676@gmail.com>
2019-11-05 06:38:20 +0700

    user column set

---
+++ src/entity/User.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+
+@Entity()
+
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    deckName: string;
+
+    @Column("text", { unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+}
