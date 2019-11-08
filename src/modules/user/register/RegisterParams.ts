cd7a5fe21b378ea3950069cd065de725c9ab1e7f
SirawichDev <sirawit0676@gmail.com>
2019-11-08 19:55:57 +0700

    seperate TypeInput

---
+++ src/modules/user/register/RegisterParams.ts
@@ -0,0 +1,28 @@
+import { InputType, Field } from 'type-graphql';
+import { Length, IsEmail } from 'class-validator';
+import { IsEmailAlreadyExist } from './emailAlreadyExistValidation';
+
+
+@InputType()
+export class RegisterParams {
+    @Field()
+    @Length(1, 255)
+    firstName: string;
+
+
+    @Field()
+    @Length(1, 255)
+    lastName: string;
+
+    @Field()
+    @IsEmail()
+    @IsEmailAlreadyExist({ message: "This Email is Already Exist" })
+    email: string;
+
+    @Field()
+    deck: string;
+
+    @Field()
+    password: string;
+
+}
