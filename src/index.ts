6be35a3a7b717820d5dca06af0e278d32acbd83c
SirawichDev <sirawit0676@gmail.com>
2019-11-22 20:08:23 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const main = async () => {
     const schema = await buildSchemas();
     const apolloServer = new ApolloServer({
         schema,
-        context: ({ req }: any) => ({ req })
+        context: ({ req,res }: any) => ({ req,res })
     });
 
     const app = Express()
