32f52758a036b62656a863868f236aed8db58b3e
SirawichDev <sirawit0676@gmail.com>
2019-11-22 21:21:20 +0700

    changepassword utils

--- src/modules/utils/mailer/createChangePasswordUrl.ts
+++ src/modules/utils/mailer/createChangePasswordUrl.ts
@@ -4,7 +4,7 @@ import { userForgetPasswordPrefix } from '../../constants/redisPrefixes';
 
 export const createChangePasswordUrl = async (userId: number) => {
     const tokenId = v4();
-    await redis.set(userForgetPasswordPrefix + tokenId,userId, "expire", 60 * 60 * 24)
+    await redis.set(userForgetPasswordPrefix + tokenId,userId, "ex", 60 * 60 * 24)
 
     return `http://localhost:3000/user/change-password/${tokenId}`;
 }
